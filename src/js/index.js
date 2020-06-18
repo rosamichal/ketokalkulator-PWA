468d1af77d9aababb35800276365ebf117eed2c6
Michał Rosa <michal.dew@gmail.com>
2020-06-18 22:03:16 +0200

    added fake ingredients.js and modified index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,36 @@
 import '../scss/main.scss';
 
+import {ingredients} from './ingredients'
+
 // uncomment the lines below to enable PWA
 // import {registerSW} from './pwa.js';
 // registerSW();
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+let recipeName;
+let ingredientsList;
+let $alphabet;
+let $info;
+let $infoWin;
+let $infoLose;
+let $newGame;
+
+let $wrongLetterCount;
+let $visiblePassword;
+let $hiddenPassword;
+
+
+const main = () => {
+    prepareDOMElements();
+    addEventListeners();
+}
+
+const prepareDOMElements = () => {
+}
+
+const addEventListeners = () => {
+
+}
+
+document.addEventListener('DOMContentLoaded', main);
---
+++ src/js/ingredients.js
@@ -0,0 +1,31 @@
+export const ingredients = [{
+    id: 1,
+    name: 'jajka',
+    protein: 10,
+    fat: 7,
+    carbohydrates: 2
+},,{
+    id: 2,
+    name: 'masło',
+    protein: 3,
+    fat: 82,
+    carbohydrates: 0
+},{
+    id: 3,
+    name: 'szynka',
+    protein: 23,
+    fat: 5,
+    carbohydrates: 6
+},{
+    id: 4,
+    name: 'awokado',
+    protein: 10,
+    fat: 40,
+    carbohydrates: 2
+}{
+    id: 5,
+    name: 'olej',
+    protein: 0,
+    fat: 100,
+    carbohydrates: 0
+}];
