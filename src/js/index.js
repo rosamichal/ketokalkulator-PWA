cec76468236579a5adeaa57ccbf692d21802986f
Michał Rosa <michal.dew@gmail.com>
2020-06-20 11:00:06 +0200

    added select with filtering

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import '../scss/main.scss';
 
 import {ingredients} from './ingredients'
+import {Select} from './select'
 
 // uncomment the lines below to enable PWA
 // import {registerSW} from './pwa.js';
@@ -24,6 +25,13 @@ let $hiddenPassword;
 const main = () => {
     prepareDOMElements();
     addEventListeners();
+
+    var mySelect = new Select('.input--select',{
+        filtered: 'auto',
+        filter_threshold: 7,
+        filter_placeholder: 'Wpisz nazwę składnika...'
+        
+    });
 }
 
 const prepareDOMElements = () => {
