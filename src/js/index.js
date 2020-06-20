ca7a56912e8b79819398f08125f1076b611fb767
Michał Rosa <michal.dew@gmail.com>
2020-06-20 11:16:35 +0200

    prepared basic DOM elements

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,11 @@
 import '../scss/main.scss';
 
-import {ingredients} from './ingredients'
-import {Select} from './select'
+import {
+    ingredients
+} from './ingredients'
+import {
+    Select
+} from './select'
 
 // uncomment the lines below to enable PWA
 // import {registerSW} from './pwa.js';
@@ -11,34 +15,44 @@ import {Select} from './select'
 
 let recipeName;
 let ingredientsList;
-let $alphabet;
-let $info;
-let $infoWin;
-let $infoLose;
-let $newGame;
-
-let $wrongLetterCount;
-let $visiblePassword;
-let $hiddenPassword;
+let btnAddIngredient;
+let btnSaveRecipe;
 
 
 const main = () => {
     prepareDOMElements();
     addEventListeners();
 
-    var mySelect = new Select('.input--select',{
+    var mySelect = new Select('.input--select', {
         filtered: 'auto',
         filter_threshold: 7,
         filter_placeholder: 'Wpisz nazwę składnika...'
-        
+
     });
 }
 
 const prepareDOMElements = () => {
+    recipeName = document.querySelector('.js--recipe-name');
+    ingredientsList = document.querySelector('.js--ingredients-list');
+    btnAddIngredient = document.querySelector('.js--btn-add-ingredient');
+    btnSaveRecipe = document.querySelector('.js--btn-save-recipe');
 }
 
 const addEventListeners = () => {
+    btnSaveRecipe.addEventListener('click', addRecipe);
+    btnAddIngredient.addEventListener('click', addIngredient);
+}
 
+const addRecipe = () => {
+
+}
+
+const addIngredient = () => {
+
+}
+
+const deleteIngredient = () => {
+    
 }
 
 document.addEventListener('DOMContentLoaded', main);
