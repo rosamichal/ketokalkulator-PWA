e6aaad002937e003ee1a3b27e718cb2f2ae2c7b4
Michał Rosa <michal.dew@gmail.com>
2020-06-20 13:25:44 +0200

    added implementation to addIngredient function

--- src/js/index.js
+++ src/js/index.js
@@ -22,13 +22,6 @@ let btnSaveRecipe;
 const main = () => {
     prepareDOMElements();
     addEventListeners();
-
-    var mySelect = new Select('.input--select', {
-        filtered: 'auto',
-        filter_threshold: 7,
-        filter_placeholder: 'Wpisz nazwę składnika...'
-
-    });
 }
 
 const prepareDOMElements = () => {
@@ -43,16 +36,73 @@ const addEventListeners = () => {
     btnAddIngredient.addEventListener('click', addIngredient);
 }
 
+const fillIngredientsSelect = select => {
+    ingredients.map(el => {
+        const optionIngredient = document.createElement('option');
+        optionIngredient.value = el.id;
+        optionIngredient.text = el.name;
+
+        select.appendChild(optionIngredient);
+    })
+}
+
 const addRecipe = () => {
 
 }
 
 const addIngredient = () => {
+    const li = document.createElement('li');
+    li.classList.add('ingredients-list__item');
 
+    const inputWeight = document.createElement('input');
+    inputWeight.type = "number";
+    inputWeight.classList.add('ingredients-list__ingredient-weight', 'input');
+    li.appendChild(inputWeight);
+
+    const selectIngredients = document.createElement('select');
+    selectIngredients.classList.add('ingredients-list__ingredient', 'input', 'input--select');
+    fillIngredientsSelect(selectIngredients);
+    li.appendChild(selectIngredients);
+    applySelectFilter(selectIngredients);
+
+    const inputProtein = document.createElement('input');
+    inputProtein.classList.add('ingredients-list__macro', 'ingredients-list__macro--protein', 'input');
+    inputProtein.disabled = true;
+    li.appendChild(inputProtein);
+
+    const inputFat = document.createElement('input');
+    inputFat.classList.add('ingredients-list__macro', 'ingredients-list__macro--fat', 'input');
+    inputFat.disabled = true;
+    li.appendChild(inputFat);
+
+    const inputCarbohydrates = document.createElement('input');
+    inputCarbohydrates.classList.add('ingredients-list__macro', 'ingredients-list__macro--carbohydrates', 'input');
+    inputCarbohydrates.disabled = true;
+    li.appendChild(inputCarbohydrates);
+
+    const inputEnergy = document.createElement('input');
+    inputEnergy.classList.add('ingredients-list__macro', 'ingredients-list__macro--energy', 'input');
+    inputEnergy.disabled = true;
+    li.appendChild(inputEnergy);
+
+    const btnDelete = document.createElement('button');
+    btnDelete.textContent = "Usuń"
+    btnDelete.addEventListener('click', deleteIngredient);
+    li.appendChild(btnDelete);
+
+    ingredientsList.appendChild(li);
+}
+
+const applySelectFilter = select => {
+    var mySelect = new Select(select, {
+        filtered: 'auto',
+        filter_threshold: 7,
+        filter_placeholder: 'Wpisz nazwę składnika...'
+    });
 }
 
 const deleteIngredient = () => {
-    
+
 }
 
 document.addEventListener('DOMContentLoaded', main);
