40013e76aed4e0df06c4bacb68edae0634bad9d0
Michał Rosa <michal.dew@gmail.com>
2020-06-24 00:14:48 +0200

    added summary macro implementation

--- src/js/index.js
+++ src/js/index.js
@@ -17,6 +17,29 @@ let recipeName;
 let ingredientsList;
 let btnAddIngredient;
 let btnSaveRecipe;
+const currentRecipe = {
+    name: "",
+    ingredients: [],
+    getProtein() {
+        return this.ingredients.reduce((proteinAll, item) => proteinAll + item.weight * item.ingredient.Protein / 100, 0).toFixed(2)
+    },
+    getFat() {
+        return this.ingredients.reduce((fatAll, item) => fatAll + item.weight * item.ingredient.Fat / 100, 0).toFixed(2)
+    },
+    getCarbohydrates() {
+        return this.ingredients.reduce((carbohydratesAll, item) => carbohydratesAll + item.weight * item.ingredient.Carbohydrates / 100, 0).toFixed(2)
+    },
+    getEnergy() {
+        return this.ingredients.reduce((energyAll, item) => energyAll + (((item.ingredient.Protein + item.ingredient.Carbohydrates) * 4 + item.ingredient.Fat * 9) * item.weight / 100), 0).toFixed(2)
+    },
+    getRatio() {
+        const p = +this.getProtein();
+        const c = +this.getCarbohydrates();
+        const f = +this.getFat();
+        
+        return f === 0 ? '0.00' : (p + c) === 0 ? 'sam tłuszcz' : (f / (p + c)).toFixed(2)
+}
+};
 
 
 const main = () => {
@@ -95,6 +118,10 @@ const addIngredient = () => {
     li.appendChild(btnDelete);
 
     ingredientsList.appendChild(li);
+    currentRecipe.ingredients.push({
+        weight: 0,
+        ingredient: getIngredients()[0]
+    })
 }
 
 const applySelectFilter = select => {
@@ -107,7 +134,13 @@ const applySelectFilter = select => {
 
 const deleteIngredient = (event) => {
     const li = event.target.closest('li');
+    
+    const ingredientIndex = Array.from(ingredientsList.children).indexOf(li);
+    delete currentRecipe.ingredients[ingredientIndex];
+    
     ingredientsList.removeChild(li);
+
+    updateRecipeMacro();
 }
 
 const updateIngredientMacro = event => {
@@ -120,10 +153,32 @@ const updateIngredientMacro = event => {
     const energyInput = li.querySelector('.ingredients-list__macro--energy');
     const ingredient = getIngredients().find(i => i.Id == ingredientSelect.value);
 
+    const ingredientIndex = Array.from(ingredientsList.children).indexOf(li);
+    currentRecipe.ingredients[ingredientIndex] = {
+        weight: weightInput.value,
+        ingredient
+    };
+
     proteinInput.value = (ingredient.Protein * weightInput.value / 100).toFixed(2);
     fatInput.value = (ingredient.Fat * weightInput.value / 100).toFixed(2);
     carbohydratesInput.value = (ingredient.Carbohydrates * weightInput.value / 100).toFixed(2);
     energyInput.value = (((ingredient.Protein + ingredient.Carbohydrates) * 4 + ingredient.Fat * 9) * weightInput.value / 100).toFixed(2);
+
+    updateRecipeMacro();
+}
+
+const updateRecipeMacro = () => {
+    const recipeRatio = document.querySelector('.summary__macro--ratio');
+    const recipeEnergy = document.querySelector('.summary__macro--energy');
+    const recipeProtein = document.querySelector('.summary__macro--protein');
+    const recipeFat = document.querySelector('.summary__macro--fat');
+    const recipeCarbohydrates = document.querySelector('.summary__macro--carbohydrates');
+
+    recipeRatio.value = currentRecipe.getRatio();
+    recipeEnergy.value = currentRecipe.getEnergy();
+    recipeProtein.value = currentRecipe.getProtein();
+    recipeFat.value = currentRecipe.getFat();
+    recipeCarbohydrates.value = currentRecipe.getCarbohydrates();
 }
 
 document.addEventListener('DOMContentLoaded', main);
